97d849c9da03e79b7a856645a04db19be6f1f6d5
Pavel Besedinsky <pavel_besedinck@mail.ru>
2024-04-02 23:19:44 +0300

    feat: Добавление базового контроллера.

---
+++ Pilotiv.AuthorizationAPI.WebUI/Controllers/ApiControllerBase.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pilotiv.AuthorizationAPI.WebUI.Controllers;
+
+/// <summary>
+/// Базовый контроллер.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    /// <summary>
+    /// Медиатор.
+    /// </summary>
+    protected IMediator Mediator { get; }
+
+    /// <summary>
+    /// Создание базового контроллера.
+    /// </summary>
+    /// <param name="mediator">Медиатор</param>
+    protected ApiControllerBase(IMediator mediator)
+    {
+        Mediator = mediator;
+    }
+}
