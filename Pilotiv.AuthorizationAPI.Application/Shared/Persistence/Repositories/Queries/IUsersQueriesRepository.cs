f47bab8681e4211c7ffbb3650ffabbaa7d8bd099
Pavel Besedinsky <pavel_besedinck@mail.ru>
2024-04-09 21:26:40 +0300

    feat: Добавлены тестовые методы в репозиторий запросов пользователей

--- Pilotiv.AuthorizationAPI.Application/Shared/Persistence/Repositories/Queries/IUsersQueriesRepository.cs
+++ Pilotiv.AuthorizationAPI.Application/Shared/Persistence/Repositories/Queries/IUsersQueriesRepository.cs
@@ -12,12 +12,14 @@ public interface IUsersQueriesRepository
     /// <summary>
     /// Получение пользователя по логину.
     /// </summary>
+    /// <param name="login">Логин пользователя.</param>
     /// <returns>Пользователь.</returns>
-    Task<Result<User>> GetUserByLoginAsync();
+    Task<Result<User>> GetUserByLoginAsync(UserLogin login);
     
     /// <summary>
     /// Получение пользователя по электронному адресу.
     /// </summary>
+    /// <param name="email">Адрес электронной почты пользователя.</param>
     /// <returns>Пользователь.</returns>
     Task<Result<User>> GetUserByEmailAsync(UserEmail email);
 }
---
+++ Pilotiv.AuthorizationAPI.Infrastructure/Persistence/Repositories/Queries/UsersQueriesRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using FluentResults;
+using Pilotiv.AuthorizationAPI.Application.Shared.Persistence.Repositories.Queries;
+using Pilotiv.AuthorizationAPI.Domain.Models.Users;
+using Pilotiv.AuthorizationAPI.Domain.Models.Users.ValueObjects;
+using Pilotiv.AuthorizationAPI.Infrastructure.Daos.Users;
+using Pilotiv.AuthorizationAPI.Infrastructure.Fabrics.Users;
+using Pilotiv.AuthorizationAPI.Infrastructure.Persistence.Context;
+
+namespace Pilotiv.AuthorizationAPI.Infrastructure.Persistence.Repositories.Queries;
+
+/// <summary>
+/// Репозиторий запросов пользователей.
+/// </summary>
+public class UsersQueriesRepository : IUsersQueriesRepository
+{
+    private readonly DbContext _dbContext;
+
+    /// <summary>
+    /// Создание репозитория запросов пользователей.
+    /// </summary>
+    /// <param name="dbContext">Контекст БД.</param>
+    public UsersQueriesRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    /// <inheritdoc />
+    public async Task<Result<User>> GetUserByLoginAsync(UserLogin login)
+    {
+        using var connection = _dbContext.CreateOpenedConnection();
+        const string sql = @"SELECT * From Users WHERE Login = @Login";
+        
+        var userDao = await connection.QuerySingleOrDefaultAsync<UserDao>(sql, new {Login = login.Value});
+        if (userDao is null)
+        {
+            throw new NotImplementedException();
+        }
+
+        var usersFabric = new UsersFabric(userDao);
+        return usersFabric.Restore(userDao.Id);
+    }
+
+    /// <inheritdoc />
+    public async Task<Result<User>> GetUserByEmailAsync(UserEmail email)
+    {
+        using var connection = _dbContext.CreateOpenedConnection();
+        const string sql = @"SELECT * From Users WHERE Email = @Email";
+        
+        var userDao = await connection.QuerySingleOrDefaultAsync<UserDao>(sql, new {Email = email.Value});
+        if (userDao is null)
+        {
+            throw new NotImplementedException();
+        }
+
+        var usersFabric = new UsersFabric(userDao);
+        return usersFabric.Restore(userDao.Id);
+    }
+}
